918d70eea948d83416cd8b1a93c7c53abd1d0489
makayru <124614484+makayru@users.noreply.github.com>
2023-04-12 16:03:03 +0200

    STYLE1
    made a color pallet, converted the site to use bootstrap, and changed all forms into functional bootstrap forms. adjusted the edit profile

--- django_eindopdracht/django_eindopdracht/urls.py
+++ django_eindopdracht/django_eindopdracht/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('myprofile/', views.MyProfile, name='myprofile'),
     path('edit_profile/<int:pk>/', views.edit_profile, name='edit_profile'),
+    path('my_profile/<int:pk>/', views.edit_profile, name='my_profile'),
     path('addnewbook/', views.AddNewBooks, name='addnewbook'),
     path('books/', views.AllBooks, name='books'),
 
