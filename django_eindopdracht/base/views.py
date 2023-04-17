c680e31263478cd031b285cf5f3ceeebac4d73ee
makayru <124614484+makayru@users.noreply.github.com>
2023-04-17 18:38:17 +0200

    merged FUNC7 with main

--- django_eindopdracht/base/views.py
+++ django_eindopdracht/base/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import login
