375496645e5f826d183fa4241e407a8884efc94b
valenciomuskiet <valenciomuskiet.ict@gmail.com>
2023-04-18 00:56:20 +0200

    Func 9 10 11
    Func 9 : admin kan boek toevoegen en deze wordt direct goedgekeurd
    Func 10: admin kan een boek verwijderen en aanpassen
    Func 11: admin kan read actions verwijderen (niet aanpassen

--- django_eindopdracht/django_eindopdracht/urls.py
+++ django_eindopdracht/django_eindopdracht/urls.py
@@ -19,27 +19,35 @@ from base import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.index, name='index'),
-    path("", include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
-    path('myprofile/', views.MyProfile, name='myprofile'),
-    path('edit_profile/<int:pk>/', views.edit_profile, name='edit_profile'),
-    path('my_profile/<int:pk>/', views.edit_profile, name='my_profile'),
-    path('addnewbook/', views.AddNewBooks, name='addnewbook'),
-    path('addreadaction/', views.AddReadAction, name='addreadaction'),
-    path('addreadaction/<int:pk>', views.EditReadAction, name='edit_readaction'),
-    path('deletereadaction/<int:pk>', views.DeleteReadAction, name='delete_readaction'),
-    path('newsfeed', views.News_feed, name='newsfeed'),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
+    path("myprofile/", views.MyProfile, name="myprofile"),
+    path("edit_profile/<int:pk>/", views.edit_profile, name="edit_profile"),
+    path("my_profile/<int:pk>/", views.edit_profile, name="my_profile"),
+    path("addnewbook/", views.AddNewBooks, name="addnewbook"),
+    path("add_newbook/", views.AddNewBooksAdmin, name="add_newbook"),
+
+
+    path("admin_books", views.AllBooksAdmin, name="admin_books"),
+    path("delete_book/<int:pk>", views.DeleteBooksAdmin, name="delete_book"),
+    path("edit_book/<int:pk>", views.EditBooksAdmin, name="edit_book"),
+
+    path("admin_readactions", views.AdminReadactions, name="admin_readactions" ),
+    path("delete_read/<int:pk>", views.DeleteReadActions, name="delete_read"),
 
 
 
 
-    path('books/', views.AllBooks, name='books'),
-    path('myreadactions/', views.MyReadActions, name='myreadactions'),
-    path('unapprovedbooks/', views.UnapprovedBooks, name='unapprovedbooks'),
-    path('approve/<int:book_id>/', views.Approve_book, name='approve_book'),
-    path('change-password/', views.change_password, name='change_password'),
-    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-
+    path("addreadaction/", views.AddReadAction, name="addreadaction"),
+    path("addreadaction/<int:pk>", views.EditReadAction, name="edit_readaction"),
+    path("deletereadaction/<int:pk>", views.DeleteReadAction, name="delete_readaction"),
+    path("newsfeed", views.News_feed, name="newsfeed"),
+    path("books/", views.AllBooks, name="books"),
+    path("myreadactions/", views.MyReadActions, name="myreadactions"),
+    path("unapprovedbooks/", views.UnapprovedBooks, name="unapprovedbooks"),
+    path("approve/<int:book_id>/", views.Approve_book, name="approve_book"),
+    path("change-password/", views.change_password, name="change_password"),
+    path("book/<int:book_id>/", views.book_detail, name="book_detail"),
 ]
